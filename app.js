8c0e653b21582127b09e10e16b3f1581d1607845
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-11 00:38:03 +0530

    setting up app.js and node modules

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const app = express()
+
 console.log('Task Manager App')
+
+const port = 3000
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("Task Manager")
+})
+
+app.listen(port,console.log(`Server is running on port ${port}`))
