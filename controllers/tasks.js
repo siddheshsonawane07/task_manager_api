25281a32af8c81d02ccd08867a5ea6f02836fd82
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-13 19:42:31 +0530

    delete task api done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,15 +12,16 @@ const getAllTasks = async (req, res) => {
 
 const getSingleTask = async (req, res) => {
     try {
-        const {id:taskID} = req.params
-        const task = await Task.findOne({_id:taskID});
-        if(!task){
-            return res.status(404).json({msg:`no task with id ${taskID}`})
+        const { id: taskID } = req.params
+        const task = await Task.findOne({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `no task with id ${taskID}` })
         }
         res.status(200).json({ task })
     } catch (err) {
         res.status(500).json({ msg: err })
-    }}
+    }
+}
 
 const createTask = async (req, res) => {
 
@@ -36,8 +37,18 @@ const editTask = (req, res) => {
     res.send('edit task')
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        res.status(201).json({ task })
+        if (!task) {
+            return res.status(404).json({ msg: `no task with id ${taskID}` })
+        }
+        res.status(200).send()
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
 
