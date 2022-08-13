2dd410d601a6ddf257562499dc23c72ba562fd39
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-13 19:32:24 +0530

    get single task api done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -10,9 +10,17 @@ const getAllTasks = async (req, res) => {
     }
 }
 
-const getSingleTask = (req, res) => {
-    res.json({ id: req.params.id })
-}
+const getSingleTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`no task with id ${taskID}`})
+        }
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }}
 
 const createTask = async (req, res) => {
 
@@ -31,6 +39,7 @@ const editTask = (req, res) => {
 const deleteTask = (req, res) => {
     res.send('delete task')
 }
+
 
 module.exports = {
     getAllTasks,
