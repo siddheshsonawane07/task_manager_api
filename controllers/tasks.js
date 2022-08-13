bbc7a9081b0b5b4aeef1e9fe3c8ad0e8d0d99d77
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-13 20:01:41 +0530

    update task done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -33,8 +33,25 @@ const createTask = async (req, res) => {
     }
 }
 
-const editTask = (req, res) => {
-    res.send('edit task')
+const editTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!task) {
+            return res.status(404).json({ msg: `no task with id ${taskID}` })
+        }
+
+        res.status(200).json({ task })
+
+    }
+    catch (err) {
+        res.status(500).json({ msg: err })
+    }
+
+
 }
 
 const deleteTask = async (req, res) => {
