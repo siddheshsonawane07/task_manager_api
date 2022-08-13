b9aba99d9d5498da9eff62df826526f28c8f2139
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-13 19:26:08 +0530

    get all tasks api done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,20 +1,26 @@
 const Task = require('../models/models')
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
 const getSingleTask = (req, res) => {
-    res.json({id:req.params.id})
+    res.json({ id: req.params.id })
 }
 
 const createTask = async (req, res) => {
 
-    try{
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-    } catch(err){
-        res.status(500).json({msg:err})
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
     }
 }
 
