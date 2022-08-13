67b85f5f12d10c4bf13cb37c9c58057988ba3480
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-13 21:53:13 +0530

    error handling

--- app.js
+++ app.js
@@ -5,28 +5,26 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 
+//middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//routes
+app.use('/api/v1/tasks',tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 const port = 3000
 
 const start = async ()=>{
     try{
         await connectDB(process.env.MONGO_URI)
-        app.listen(port,console.log(`Server is listening on port ${port}`))
-
+        app.listen(port,console.log(`Server is listening on port ${port}`));
     }catch(err){
         console.log(err);
     }
-}
-
-//middleware
-app.use(express.static('./public'))
-app.use(express.json())
-app.use(notFound)
-
-
-//routes
-app.use('/api/v1/tasks',tasks)
-
+};
 
 start()
